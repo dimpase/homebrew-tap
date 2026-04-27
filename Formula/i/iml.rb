19775139326dc26c6eafd28cc513d1232533b1d3
Dima Pasechnik <dimpase@gmail.com>
2026-04-27 18:21:25 +0000

    iml: add 1.0.5 bottle.

--- Formula/i/iml.rb
+++ Formula/i/iml.rb
@@ -5,6 +5,13 @@ class Iml < Formula
   sha256 "1dad666850895a5709b00b97422e2273f293cfadea7697a9f90b90953e847c2a"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/iml-1.0.5"
+    sha256 cellar: :any,                 arm64_tahoe:  "69d5702816e0a371ea4d4fb3b8d749b40217ae558c73263f4ffe5fc179955b2b"
+    sha256 cellar: :any,                 sequoia:      "9256dc4043ea434da41783ef84d0639091923877222dae3da058fa78d0b301cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1024f70368a5bed23f06714caa638975685b39c20e385b2892a9c5a5dc4f8a50"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
