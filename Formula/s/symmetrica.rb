fee68128b1b2971b5e1c3ba58052ce1f4889ff82
Dima Pasechnik <dimpase@gmail.com>
2026-04-27 19:05:24 +0000

    symmetrica: add 3.1.0 bottle.

--- Formula/s/symmetrica.rb
+++ Formula/s/symmetrica.rb
@@ -5,6 +5,14 @@ class Symmetrica < Formula
   sha256 "2efc4b1d047c6fe036453882a20e906ca2bedecae888356c7626447db2b10fd3"
   license :public_domain
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/symmetrica-3.1.0"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:  "6df1998999175c55e36e978732044f0d88115053f9b9d898f2ab8b4a1606b9c1"
+    sha256 cellar: :any,                 sequoia:      "615cdeee7f43596317af3cbdd24a1c825e6465194bf4adebf2d4ea3d4329fe44"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6a733c1b677feda97abac67aa960d632a0b7b350109ef2fa0a86a458f94cd7a9"
+  end
+
   depends_on "libtool" => :build
 
   def install
