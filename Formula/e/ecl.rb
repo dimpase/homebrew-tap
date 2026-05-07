efd4205546a95b7c9f4b0ac1dd84e9f36956c26c
Dima Pasechnik <dimpase@gmail.com>
2026-05-07 21:43:10 +0000

    ecl: add 26.5.5 bottle.

--- Formula/e/ecl.rb
+++ Formula/e/ecl.rb
@@ -11,6 +11,12 @@ class Ecl < Formula
     regex(/href=.*?ecl[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/ecl-26.5.5"
+    sha256 arm64_tahoe:  "3ecd2905ee8d48e9743557a0aecf09ee37008b85d1b4ae16453fc07e7bce8724"
+    sha256 x86_64_linux: "0e07dee7a3f242310037fe2b7b8fe7500f4fe54ba6f011bafb2a374bdcafb691"
+  end
+
   depends_on "texinfo" => :build # Apple's is too old
   depends_on "bdw-gc"
   depends_on "gmp"
