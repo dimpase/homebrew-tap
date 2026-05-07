2d7baf14b85e27c6767c906ff7552c3e32e9698e
Dima Pasechnik <dima@pasechnik.info>
2026-05-07 14:13:02 -0700

    ecl 24.5.10 from homebrew-core, changed to 26.5.5
    removed bottle block in anticipation of getting new ones
    
    Closes #21.
    
    Signed-off-by: Dima Pasechnik <dimpase@gmail.com>

---
+++ Formula/e/ecl.rb
@@ -0,0 +1,44 @@
+class Ecl < Formula
+  desc "Embeddable Common Lisp"
+  homepage "https://ecl.common-lisp.dev"
+  url "https://ecl.common-lisp.dev/static/files/release/ecl-26.5.5.tgz"
+  sha256 "a01a5bcda8c5b73e59dda3494fd13e5fec5db6aa1dad782c3cc3bb57f1633435"
+  license "LGPL-2.1-or-later"
+  head "https://gitlab.com/embeddable-common-lisp/ecl.git", branch: "develop"
+
+  livecheck do
+    url "https://ecl.common-lisp.dev/static/files/release/"
+    regex(/href=.*?ecl[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "texinfo" => :build # Apple's is too old
+  depends_on "bdw-gc"
+  depends_on "gmp"
+  uses_from_macos "libffi"
+
+  def install
+    ENV.deparallelize
+
+    libffi_prefix = if OS.mac?
+      MacOS.sdk_path
+    else
+      Formula["libffi"].opt_prefix
+    end
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-threads=yes",
+                          "--enable-boehm=system",
+                          "--enable-gmp=system",
+                          "--with-gmp-prefix=#{Formula["gmp"].opt_prefix}",
+                          "--with-libffi-prefix=#{libffi_prefix}",
+                          "--with-libgc-prefix=#{Formula["bdw-gc"].opt_prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"simple.cl").write <<~LISP
+      (write-line (write-to-string (+ 2 2)))
+    LISP
+    assert_equal "4", shell_output("#{bin}/ecl -shell #{testpath}/simple.cl").chomp
+  end
+end
