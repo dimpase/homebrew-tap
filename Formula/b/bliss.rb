8abeaae0f16c3a5e38e3319660683d9f57ab6cc7
Dima Pasechnik <dimpase@gmail.com>
2026-05-11 15:50:35 +0000

    bliss: add 0.77 bottle.

--- Formula/b/bliss.rb
+++ Formula/b/bliss.rb
@@ -5,6 +5,13 @@ class Bliss < Formula
   sha256 "acc8b98034f30fad24c897f365abd866c13d9f1bb207e398d0caf136875972a4"
   license "LGPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/bliss-0.77"
+    sha256 cellar: :any,                 arm64_tahoe:  "445e5a788ef2deda1d3cb313295c097eef298b3be8675c7d857fa2980c5a2d3a"
+    sha256 cellar: :any,                 sequoia:      "0bc5164c6985847487392b0ee07802279e8d2e536d05d38dc932b19691d55d07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "821919cd37c2a3433b3fdbdc4afe3e3132516e0a5673c85115ffa3a7d35f0f7b"
+  end
+
   depends_on "cmake" => :build
 
   patch :DATA
