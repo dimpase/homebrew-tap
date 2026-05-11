5d144ba8b9428d881f97ef7097ff53c943809c19
Dima Pasechnik <dimpase@gmail.com>
2026-05-11 15:50:35 +0000

    sirocco: add 2.1.1 bottle.

--- Formula/s/sirocco.rb
+++ Formula/s/sirocco.rb
@@ -5,6 +5,13 @@ class Sirocco < Formula
   sha256 "83d1dd5622120eda42c475696235dd67be8072a76baad0a70693d9743a659a61"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/sirocco-2.1.1"
+    sha256 cellar: :any,                 arm64_tahoe:  "045940e1eeaa52fe2e44972b0bc61728484357cf8d550832edf14b4e545c5f9b"
+    sha256 cellar: :any,                 sequoia:      "df4c8884b0d8fc43c095761311b2e917e9cc5eb9efe5f1f1a6182b0a0c0c40a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1177482b43470df1f1c0e3c28f06ee7459178565a57681e2836652ebaf065408"
+  end
+
   depends_on "libtool" => :build
   depends_on "gmp"
   depends_on "mpfr"
