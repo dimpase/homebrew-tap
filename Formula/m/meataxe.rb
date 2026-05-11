08fafad2157f15a84e2babba5ef052a1a75efe36
Dima Pasechnik <dimpase@gmail.com>
2026-05-11 06:21:15 +0000

    meataxe: add 1.0.2 bottle.

--- Formula/m/meataxe.rb
+++ Formula/m/meataxe.rb
@@ -5,6 +5,13 @@ class Meataxe < Formula
   sha256 "c2e2ec85cdbcde5800d7b0577937afcc86f4a7610ae4d86614428445deb301e8"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/meataxe-1.0.2"
+    sha256 cellar: :any,                 arm64_tahoe:  "482f0c606a4547e34fdedac352326c3a12596bfbdf5ecbe92c9846acb25db69a"
+    sha256 cellar: :any,                 sequoia:      "43a03d954d359cdd99b363bcc61c21e4d7f323106bc1cacaf30b0b0d69b3886d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c304b177a8ec83ab0336800edc31dd0e9df0228eae46631faaacbd34912a2f0f"
+  end
+
   depends_on "libtool" => :build
 
   def install
