8f70964c49780e97a31107655608243c84e95897
Dima Pasechnik <dima@pasechnik.info>
2026-05-20 12:54:30 -0700

    patch to be able to disable debug output

--- Formula/b/brial.rb
+++ Formula/b/brial.rb
@@ -4,7 +4,10 @@ class Brial < Formula
   url "https://github.com/BRiAl/BRiAl/releases/download/1.2.15/brial-1.2.15.tar.bz2"
   sha256 "1d0e7de01b1b60ad892dee6c02503f5932e2ed211f3f8dbdf1a3216c696ef201"
   license all_of: ["0BSD", "GPL-2.0-only"]
+  revision 1
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "boost"
   depends_on "libomp" if OS.mac?
@@ -12,7 +15,10 @@ class Brial < Formula
   depends_on "m4ri"
   depends_on "pkgconf"
 
+  patch :DATA
+
   def install
+    system "autoreconf", "-ivf"
     # Remove unrecognized options if they cause configure to fail
     # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
     system "./configure", "--with-boost=#{HOMEBREW_PREFIX}", "--with-boost-libdir=#{HOMEBREW_PREFIX}/lib",
@@ -24,3 +30,31 @@ class Brial < Formula
     system "true"
   end
 end
+
+__END__
+
+
+diff --git a/configure.ac b/configure.ac
+index f8953244..5a563134 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -36,10 +36,18 @@ AX_BOOST_BASE([1.58.0],[],
+ dnl Check that the boost unit test framework is present for running the testsuite
+ AX_BOOST_UNIT_TEST_FRAMEWORK
+ 
++AC_MSG_CHECKING([for debugging being enabled])
+ AC_ARG_ENABLE([debug],
+               [AS_HELP_STRING([--enable-debug],
+                               [enable extra debugging statements in BRiAl's code [default=no]])],
+-              [AC_DEFINE([PBORI_DEBUG],[],[enable extra debugging statements])])
++              [if test "$enableval" = "yes"; then
++                   AC_DEFINE([PBORI_DEBUG],[],[enable extra debugging statements])
++                   AC_MSG_RESULT([yes])
++               else
++                   AC_MSG_RESULT([no])
++               fi
++              ], [AC_MSG_RESULT([no])])
++
+ AC_ARG_WITH([pkgconfigdir], AS_HELP_STRING([--with-pkgconfigdir=PATH],
+ 	[Path to the pkgconfig directory [[LIBDIR/pkgconfig]]]),
+ 	[pkgconfigdir="$withval"], [pkgconfigdir='${libdir}/pkgconfig'])
