10180a8a89e7043a27c81745cd92f321ddd4ba8a
Dima Pasechnik <dimpase@gmail.com>
2026-06-10 20:06:25 +0000

    lrcalc: add 2.1 bottle.

--- Formula/l/lrcalc.rb
+++ Formula/l/lrcalc.rb
@@ -5,6 +5,13 @@ class Lrcalc < Formula
   sha256 "996ac00e6ea8321ef09b34478f5379f613933c3254aeba624b6419b8afa5df57"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/lrcalc-2.1"
+    sha256 cellar: :any, arm64_tahoe:  "1c7dcf915951938ace614325551c943a627bed4d1b97bdc65bccffe552b44b26"
+    sha256 cellar: :any, sequoia:      "7378fcea994276234eb24cf6f9d45d35a3d2b8c48905954744792b22281e5db1"
+    sha256 cellar: :any, x86_64_linux: "b8b5bfe66cc279163b4e7f44e7d70a70b4d3c17bd5fe9f8e6e83a43cb1ec0353"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
