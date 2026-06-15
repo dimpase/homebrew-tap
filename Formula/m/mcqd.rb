a9f92fe36da56b04f89c1fba5e1d3186e8e19e55
Dima Pasechnik <dimpase@gmail.com>
2026-06-15 18:53:06 +0000

    mcqd: add 1.0.2 bottle.

--- Formula/m/mcqd.rb
+++ Formula/m/mcqd.rb
@@ -5,6 +5,13 @@ class Mcqd < Formula
   sha256 "2e5e368ac385f06e82cee78994b5f7646b6ea4786e12849765ccbebe226b97cd"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/mcqd-1.0.2"
+    sha256 cellar: :any, arm64_tahoe:  "42af2021aace19818fbb80968f94b083b04e14019d5b26fb5c105dccb6554a53"
+    sha256 cellar: :any, sequoia:      "a1c7b93466a3ca1d156ce82abb9c5d56ccd1b7bed130056f661505e058d9686a"
+    sha256 cellar: :any, x86_64_linux: "92cae27944d032b18e47a733c965b3959d0ba7e6c639b66cb1838f3627dd78ff"
+  end
+
   depends_on "cmake" => :build
 
   def install
