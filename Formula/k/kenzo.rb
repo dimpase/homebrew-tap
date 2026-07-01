af8d9fee1c958cdc525777349a75a3068f94c9b6
Dima Pasechnik <dimpase@gmail.com>
2026-07-01 13:14:49 +0000

    kenzo: add 1.1.10 bottle.

--- Formula/k/kenzo.rb
+++ Formula/k/kenzo.rb
@@ -5,6 +5,12 @@ class Kenzo < Formula
   sha256 "2a23697611a37714433a9e273098d0b2dfe4fdcd83d98b9fc8d7a055c064ed6b"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/dimpase/homebrew-tap/releases/download/kenzo-1.1.10"
+    sha256 cellar: :any, arm64_tahoe:  "1668e9826a2f71f0864cf9d096c8aecc2ca000f791434db76dc1fac250b2c7d5"
+    sha256               x86_64_linux: "6a6fce746852aaa1ee25e39f88b4a5d0ab95382761d3419f478406d59378d8c3"
+  end
+
   depends_on "bdw-gc"
   depends_on "ecl"
   depends_on "gmp"
