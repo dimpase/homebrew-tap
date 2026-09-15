d51beaafce3bafae148833e556ad61b46b67fccd
Dima Pasechnik <dima@pasechnik.info>
2026-09-14 21:15:28 -0500

    Update Tachyon URL to GitHub repository

--- Formula/t/tachyon.rb
+++ Formula/t/tachyon.rb
@@ -1,7 +1,8 @@
 class Tachyon < Formula
   desc "John Stone's ray tracer"
   homepage "http://jedi.ks.uiuc.edu/~johns/raytracer/"
-  url "http://jedi.ks.uiuc.edu/~johns/raytracer/files/0.99.5/tachyon-0.99.5.tar.gz"
+  url "https://github.com/dimpase/homebrew-tap/archive/refs/tags/tachyon-0.99.5.tar.gz"
+  # url "http://jedi.ks.uiuc.edu/~johns/raytracer/files/0.99.5/tachyon-0.99.5.tar.gz"
   sha256 "09203c102311149f5df5cc367409f96c725742666d19c24db5ba994d5a81a6f5"
   license "BSD-3-clause"
 
